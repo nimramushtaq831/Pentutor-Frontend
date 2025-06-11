7ff3198365e26d0b650144d0e7fa8603975a7d5d
waleed-awan <waleedcine007@gmail.com>
2025-06-11 19:56:10 +0500

    A team members page has been created.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Sidebar from "./assets/SideBar";
 import TopBar from "./assets/TopBar";
 import FeatureGrid from "./assets/FeatureGrid";
+import TeamsCalendar from "./assets/team-membering";
 
 function App() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
+    <div>
+      {/* <Sidebar />
       <div className="flex flex-col flex-1">
         <TopBar />
         <FeatureGrid />
-      </div>
+      </div> */}
+      <TeamsCalendar/>
     </div>
   );
 }
